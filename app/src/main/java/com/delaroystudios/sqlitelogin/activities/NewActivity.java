b8f9703c454297e953138ee9af909174b93409e7
imlandau <30660654+imlandau@users.noreply.github.com>
2017-12-03 23:50:44 -0500

    1

---
+++ app/src/main/java/com/delaroystudios/sqlitelogin/activities/NewActivity.java
@@ -0,0 +1,15 @@
+package com.delaroystudios.sqlitelogin.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.delaroystudios.sqlitelogin.R;
+
+public class NewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
--- app/src/main/java/com/delaroystudios/sqlitelogin/activities/UsersActivity.java
+++ app/src/main/java/com/delaroystudios/sqlitelogin/activities/UsersActivity.java
@@ -1,7 +1,9 @@
 package com.delaroystudios.sqlitelogin.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.delaroystudios.sqlitelogin.R;
@@ -13,13 +15,14 @@ public class UsersActivity extends AppCompatActivity {
 
     private TextView textViewName;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
-
         textViewName = (TextView) findViewById(R.id.text1);
         String nameFromIntent = getIntent().getStringExtra("EMAIL");
         textViewName.setText("Welcome " + nameFromIntent);
+
     }
 }
