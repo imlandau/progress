148b6cfcc3a243d7133272e6a5614872517fb957
imlandau <30660654+imlandau@users.noreply.github.com>
2017-12-03 23:59:29 -0500

    1

--- app/src/main/java/com/delaroystudios/sqlitelogin/activities/UsersActivity.java
+++ app/src/main/java/com/delaroystudios/sqlitelogin/activities/UsersActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.delaroystudios.sqlitelogin.R;
@@ -23,6 +24,13 @@ public class UsersActivity extends AppCompatActivity {
         textViewName = (TextView) findViewById(R.id.text1);
         String nameFromIntent = getIntent().getStringExtra("EMAIL");
         textViewName.setText("Welcome " + nameFromIntent);
+        Button toMenuBtn = (Button) findViewById(R.id.toMenuBtn);
 
+        toMenuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UsersActivity.this, NewActivity.class));
+            }
+        });
     }
 }
